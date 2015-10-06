628642f8a9152ed0db651f6db9e090706a22b1f2
Nicole M <nicmatts@gmail.com>
2015-10-06 17:09:51 -0500

    Add articles, static files, and templates

--- nmc_local/urls.py
+++ nmc_local/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.flatpages import views
+
+#from . import views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'nmc_local.views.home', name='home'),
+    #url(r'^$', 'articles.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^panel/', include(admin.site.urls)),
+    #url(r'^contact/$', views.contact, name='contact'),
+    #url(r'^about/$', views.flatpage, {'url': '/about/'}, name='about'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
