a3fe29617d39140382cf5c74605475893e813f9b
Nicole M <nicmatts@gmail.com>
2015-10-07 15:28:33 -0500

    Add contact URL

--- nmc_local/urls.py
+++ nmc_local/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     #url(r'^$', 'articles.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^panel/', include(admin.site.urls)),
+    url(r'^contact/$', views.contact, name='contact'),
     #url(r'^contact/$', views.contact, name='contact'),
     #url(r'^about/$', views.flatpage, {'url': '/about/'}, name='about'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
